f9200f13214fe214346c534a9dd6d24188dd5da5
Matus Muransky <matus.muransky2@gmail.com>
2020-11-13 14:29:01 +0100

    Adding post form

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,14 +1,68 @@
-import { defaultDataIdFromObject } from '@apollo/client';
 import React from 'react';
-import { Form } from 'semantic-ui-react';
+import { useMutation, gql } from '@apollo/client';
+import { Button, Form, Message } from 'semantic-ui-react';
 
 import { useForm } from '../util/hooks';
 
-function PostForm(params) {
-    const { onSubmit, onChange, values } = useForm();
+const CREATE_POST_MUTATION = gql`
+    mutation createPost($body: String!) {
+        createPost(body: $body) {
+            id body createdAt username commentsCount likesCount 
+            likes {
+                id username createdAt
+            } 
+            comments {
+                id body createdAt username
+            }
+        }
+    }
+`;
+
+function PostForm() {
+    const INITIAL_STATE = {
+        body: "",
+    }
+
+    const [errors, setErrors] = React.useState({});
+
+    const { handleSubmit, handleChange, values } = useForm(addPost, INITIAL_STATE);
+
+    const [createPost, { loading }] = useMutation(CREATE_POST_MUTATION, {
+        update(proxy, result) {
+            console.log(result);
+            values.body = '';
+        },
+        onError(err) {
+            console.log(err);
+            setErrors(err);
+        },
+        variables: values
+    });
+
+    function addPost(params) {
+        createPost();
+    }
     return(
-        <Form onSubmit={onSubmit}>
-            Post Form
+        <Form onSubmit={handleSubmit} noValidate className={loading ? "loading" : ""}>
+            <h2>Add Post</h2>
+            <Form.Input 
+                placeholder="Type your post here..."
+                name="body"
+                onChange={handleChange}
+                value={values.body}
+            />
+            <Button 
+                type="submit"
+                content="Add Post"
+                color="green"
+            />
+            {Object.keys(errors).length > 0 && (
+                <Message
+                    color="red" 
+                    compact
+                    content={errors}
+                />
+            )}
         </Form>
     );
 }
